9fbc8c7557760268ed47d9433102fe2def0c4d9e
pinyoo thotaboot <pinyoo_too@hotmail.com>
2020-04-12 14:26:55 +0700

    First Commit

---
+++ bme_pool.py
@@ -0,0 +1,155 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+import sys,os
+from os.path import dirname, join, abspath
+sys.path.insert(0, abspath(join(dirname(__file__), '..')))
+import requests
+import time
+import csv
+import xlsxwriter 
+import xlrd 
+import xlwt
+import re
+from datetime import datetime
+from lxml import html,etree
+from xlutils.copy import copy
+from config import *
+
+def doLogin():
+    try:
+        payload = {
+                'user': USERNAME,
+                'pass': PASSWORD,
+                'Submit': "Submit"
+            }
+        with requests.Session() as sess:
+            result = sess.post(LOGIN_URL,data=payload,headers=dict(referer=LOGIN_URL))
+            if result.status_code == 200:
+                return 200,sess
+            else:
+                return result.status_code,"Can not login"
+    except Exception as ex:
+        print('[LOGIN]- {}'.format(ex))
+        return None
+
+def doPoolProcess(sess,data,nextDate):
+    try:
+        doGetFormPool = sess.get(url=URL_POOL,headers=dict(referer=URL_POOL_REFERER))
+        if doGetFormPool.status_code == 200:
+            doPost = sess.post(url=URL_FIND,headers=dict(referer=URL_POOL),data=data)
+
+            if doPost.status_code == 200:
+                tree = html.fromstring(doPost.content)
+
+                reg = re.compile('((?!\r\n)\s+)')
+                ls = str(reg.sub(' ',str(doPost.content))).split(" ")
+                myText = "".join(ls)
+
+                ss = re.findall(r'<td>&nbsp;(\d[0-9]+)</td>',str(myText))
+
+                itemno_hd_1 = re.findall(r'itemno_hd=(\d[0-9]+)&',str(myText))
+
+                reserve_no = re.findall(r'reserve_no;(\d[0-9]+)"',str(myText))
+
+                url = "http://nsmart.nhealth-asia.com/MTDPDB01/reserve/reserve_back1.php?itemno_hd={}&ccsForm=tbl_reserve1".format(itemno_hd_1[0])
+                referer = "http://nsmart.nhealth-asia.com/MTDPDB01/reserve/reserve_back1.php?itemno_hd={}".format(itemno_hd_1)
+
+                packet = {
+                    "FormState": "1;0;reserve_no;{}".format(reserve_no[0]),
+                    "itemno_hd_1": itemno_hd_1[0],
+                    "hn_1": 1,
+                    "reserve_dept_1": DEPARTMENT_ID,
+                    "reserve_branchid_1":BRANCH_ID,
+                    "inform_date_1": data['requestdate'],
+                    "inform_date1_1": nextDate,
+                    "Button_Submit": "บันทึก"
+                }
+
+                result = sess.post(url=url,headers=dict(referer=referer),data=packet)
+
+                if result.status_code == 200:
+                    print("[{}] - Create Pool Successfully.".format(data['sap_code']))
+                    return True
+    
+    except Exception as ex:
+        print('[POOL]- {}'.format(ex))
+        return None
+
+def doChangeDepartment(dept):
+    return DEPT[str(dept)]
+
+def doMainProcess(sess,loc):
+    try:
+        i = 1
+        ck = True
+        while ck != False:
+            rb = xlrd.open_workbook(loc) 
+            sheet = rb.sheet_by_index(0) 
+  
+            sheet.cell_value(0, 0)
+            date_tuple = xlrd.xldate_as_tuple(sheet.row_values(i)[2], rb.datemode)
+            _date = datetime(*date_tuple).strftime('%d/%m/%Y').split('/')
+            newDate = _date[0]+'/'+_date[1]+'/'+str(int(_date[2]))
+
+            ndate_tuple = xlrd.xldate_as_tuple(sheet.row_values(i)[3], rb.datemode)
+            _ndate = datetime(*ndate_tuple).strftime('%d/%m/%Y').split('/')
+            nextDate = _ndate[0]+'/'+_ndate[1]+'/'+str(int(_ndate[2]))
+
+            now = datetime.now()
+            s2 = now.strftime("%d/%m/%Y")
+
+            data = {
+                "sap_code": sheet.row_values(i)[1],
+                "requestdate": newDate,
+                "equip_unit": 1,
+                "old_reserve": 1,
+                "groupid": "",
+                "catagory": "",
+                "reserve_branchid": BRANCH_ID,
+                "reserve_dept": DEPARTMENT_ID,
+                "reserve_sub_dept": doChangeDepartment(str(sheet.row_values(i)[4])),
+                "borrow_name": str(sheet.row_values(i)[5]).encode('tis_620'),
+                "borrow_tele": str(int(sheet.row_values(i)[6])),
+                "enterby": "Engineer",
+                "enterdate": str(s2),
+                "Button_Insert.x": 18,
+                "Button_Insert.y": 9
+            }
+
+            if sheet.row_values(i)[7] not in ["True",True]:
+                result = doPoolProcess(sess,data,nextDate)
+                if result == True:
+                    wb = copy(rb)
+                    w_sheet = wb.get_sheet(0)
+                    w_sheet.write(i,7,'True')
+                    wb.save(loc)
+                else:
+                    wb = copy(rb)
+                    w_sheet = wb.get_sheet(0)
+                    w_sheet.write(i,7,'False')
+                    wb.save(loc)
+
+            if int(sheet.row_values(i)[0]) is None:
+                ck = False
+                break
+            else:
+                i+=1
+                time.sleep(1)
+            
+    except Exception as ex:
+        print('[MAIN]- {}'.format(ex))
+
+if __name__ == "__main__":
+    try:
+        status,sess = doLogin()
+        _start = time.time()
+        if status == 200:
+            filename = input("Enter Excel file name : ")
+            loc = '{}.xls'.format(filename)
+            doMainProcess(sess,loc)
+            _end = time.time()
+            print('[END] - Total time %.2f min.'%((_end-_start)/60))
+    except KeyboardInterrupt:
+        print('[MAIN]- CQI Service stop!..')
+        sys.exit()
+    
---
+++ config.py
@@ -0,0 +1,79 @@
+LOGIN_URL = "http://nsmart.nhealth-asia.com/MTDPDB01/index.php"
+
+################ URL For Pool Create #########################
+## Step 01
+URL_POOL = "http://nsmart.nhealth-asia.com/MTDPDB01/reserve/reserve_back1.php"
+URL_POOL_REFERER = "http://nsmart.nhealth-asia.com/MTDPDB01/reserve/reserve_back.php"
+
+## Step 02
+URL_POOL_FIND_EQUIP = "http://nsmart.nhealth-asia.com/MTDPDB01/reserve/areserve_list1x.php?groupid=&catagory=&reserve_no="
+
+## Step 03
+URL_POOL_FINDED = "http://nsmart.nhealth-asia.com/MTDPDB01/reserve/areserve_list1x.php?groupid=&catagory=&reserve_no=&ccsForm=a_m_groupSearch"
+
+## Step 04
+URL_FOUND = "http://nsmart.nhealth-asia.com/MTDPDB01/reserve/reserve_back1.php?itemno_hd=775933"
+URL_FIND = "http://nsmart.nhealth-asia.com/MTDPDB01/reserve/reserve_back1.php?ccsForm=tbl_reserve"
+
+
+USERNAME = "username"
+PASSWORD = "password"
+
+DEPARTMENT_ID = "department id"
+BRANCH_ID = "branch id"
+
+DEPT = {
+    "ANE": "00018010001",
+    "BME-SSH": "00018010002",
+    "DEN": "00018010003",
+    "ER": "00018010004",
+    "GI": "00018010005",
+    "HEM": "00018010006",
+    "ICU": "00018010007",
+    "L&N": "00018010008",
+    "LAB": "00018010040",
+    "MG": "00018010009",
+    "NSO": "00018010048",
+    "Occ. med Mobile": "00018010046",
+    "OPD BRAIN": "00018010011",
+    "OPD CARDIO": "00018010012",
+    "OPD CATHLAB": "00018010013",
+    "OPD ENT": "00018010014",
+    "OPD JAPAN": "00018010015",
+    "OPD MED": "00018010016",
+    "OPD NIGHT": "00018010045",
+    "OPD OBG": "00018010017",
+    "OPD ORTHO": "00018010018",
+    "OPD PED": "00018010019",
+    "OPD SUR": "00018010020",
+    "OPR": "00018010021",
+    "PHA (คลังยา)": "00018010023",
+    "PHA (ห้องยา)": "00018010022",
+    "RAD": "00018010024",
+    "REH": "00018010025",
+    "SEI": "00018010026",
+    "SSH Interpreter": "00018011061",
+    "STU": "00018010027",
+    "URO": "00018010047",
+    "W.10": "00018010028",
+    "W.5": "00018010029",
+    "W.6": "00018010030",
+    "W.7": "00018010031",
+    "W.8": "00018010032",
+    "W.9": "00018010033",
+    "W.C12 (JMC)": "00018010043",
+    "W.C2": "00018010044",
+    "W.C3": "00018010053",
+    "W.C5": "00018010041",
+    "W.C6": "00018010042",
+    "W.C8": "00018014031",
+    "W.C9": "00018010050",
+    "WELLNESS CENTER": "00018010034",
+    "คลินิก ESSO": "00018010051",
+    "คลินิก J-PARK": "00018010049",
+    "คลินิก บ่อวิน": "00018010036",
+    "คลินิก บ้านบึง": "00018010039",
+    "คลินิก มิตซู": "00018010052",
+    "คลินิก สหพัฒน์": "00018010037"
+}
+
